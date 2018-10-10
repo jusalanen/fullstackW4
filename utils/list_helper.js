f919d510f3447654a369cfb4097359119008f8d9
Jussi Alanen <jussi.alanen@fonet.fi>
2018-10-10 17:06:03 +0300

    lisää listHelper.test.js

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,16 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const arr = [].concat(blogs)
+  let likes = 0
+  arr.forEach(blog => {
+    likes += blog.likes
+  })
+  return likes
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
